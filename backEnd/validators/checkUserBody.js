69633e300784e3f92832b6d09238ae0ff4602185
D1JS <d1js@TECHIN.INTERNAL>
2025-05-07 15:40:00 +0300

    Add better sign up validation

--- backEnd/validators/checkUserBody.js
+++ backEnd/validators/checkUserBody.js
@@ -1,8 +1,28 @@
 const { body, checkExact } = require("express-validator");
+const { User } = require("../models/userModel");
 
 exports.checkSignupBody = [
-  body("email").trim().notEmpty().isEmail().normalizeEmail(),
+  body("email")
+    .trim()
+    .notEmpty()
+    .isEmail()
+    .normalizeEmail()
+    .custom(async (value) => {
+      const user = await User.findOne({ where: { email: value } });
+      if (user) {
+        throw new Error("Email already in use");
+      }
+      return true;
+    }),
+
   body("password").trim().notEmpty().isLength({ min: 8, max: 20 }),
+
+  body("passwordConfirm").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Passwords do not match");
+    }
+    return true;
+  }),
 
   checkExact([], {
     message: (fields) =>
