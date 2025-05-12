f9dc286801a0dce2eb9d36e9cec3d57e4bee22c6
13urn0ut <nitrojunky@gmail.com>
2025-05-12 17:54:28 +0300

    Add item body validation

--- backEnd/routes/itemRouter.js
+++ backEnd/routes/itemRouter.js
@@ -7,13 +7,32 @@ const {
   deleteItem,
 } = require("../controllers/itemController");
 const { protect, allowAccessTo } = require("../controllers/userController");
+const {
+  checkCreateItemBody,
+  checkUpdateItemBody,
+} = require("../validators/checkItemBody");
+const validate = require("../validators/validate");
 
 itemRouter.route("/").get(getAllItems);
-itemRouter.route("/").post(protect, allowAccessTo("admin"), createItem);
+itemRouter
+  .route("/")
+  .post(
+    protect,
+    allowAccessTo("admin"),
+    checkCreateItemBody,
+    validate,
+    createItem
+  );
 itemRouter
   .route("/:id")
   .get(getItemById)
-  .patch(protect, allowAccessTo("admin"), updateItem)
+  .patch(
+    protect,
+    allowAccessTo("admin"),
+    checkUpdateItemBody,
+    validate,
+    updateItem
+  )
   .delete(protect, allowAccessTo("admin"), deleteItem);
 
 module.exports = itemRouter;
---
+++ backEnd/validators/checkItemBody.js
@@ -0,0 +1,69 @@
+const { body, checkExact } = require("express-validator");
+const { Item } = require("../models");
+
+exports.checkCreateItemBody = [
+  body("name")
+    .trim()
+    .notEmpty()
+    .withMessage("Item name is required")
+    .custom(async (value) => {
+      const item = await Item.findOne({ where: { name: value } });
+
+      if (item) {
+        throw new Error("Item name already exists");
+      }
+      return true;
+    }),
+
+  body("price").trim().notEmpty().withMessage("Item price is required"),
+
+  body("categoryId").trim().isInt().withMessage("Item category is required"),
+
+  body("duration").trim().isInt().withMessage("Item duration is required"),
+
+  body("image").trim().notEmpty().withMessage("Item image is required"),
+
+  checkExact([], {
+    message: (fields) =>
+      fields
+        .map(
+          (field) =>
+            `Unknown field ${field.path} in ${field.location} with value ${field.value}`
+        )
+        .join("; "),
+  }),
+];
+
+exports.checkUpdateItemBody = [
+  body("name")
+    .trim()
+    .optional()
+    .custom(async (value, { req }) => {
+      const item = await Item.findOne({ where: { name: value } });
+
+      console.log(item.id, req.params.id);
+
+      if (item && item.id !== +req.params.id) {
+        throw new Error("Item name already exists");
+      }
+      return true;
+    }),
+
+  body("price").trim().optional(),
+
+  body("categoryId").trim().optional(),
+
+  body("duration").trim().optional(),
+
+  body("image").trim().optional(),
+
+  checkExact([], {
+    message: (fields) =>
+      fields
+        .map(
+          (field) =>
+            `Unknown field ${field.path} in ${field.location} with value ${field.value}`
+        )
+        .join("; "),
+  }),
+];
