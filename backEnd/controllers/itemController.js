ebe429445c06141b48acbdfcf4a53b4ffbbd56a3
13urn0ut <nitrojunky@gmail.com>
2025-05-12 17:36:52 +0300

    Change controllers

--- backEnd/controllers/itemController.js
+++ backEnd/controllers/itemController.js
@@ -1,12 +1,16 @@
-const { Item, Category, Date } = require("../models");
+const { Item, Category } = require("../models");
 const AppError = require("../utils/appError");
 
 exports.getAllItems = async (req, res, next) => {
   try {
     const items = await Item.findAll({
-      include: [{ model: Category }, { model: Date }],
+      include: [{ model: Category }],
     });
-    res.status(200).json({ status: "success", data: items });
+    const itemCount = await Item.count();
+
+    res
+      .status(200)
+      .json({ status: "success", results: itemCount, data: items });
   } catch (error) {
     next(new AppError(error.message, 400));
   }
@@ -15,7 +19,7 @@ exports.getAllItems = async (req, res, next) => {
 exports.getItemById = async (req, res, next) => {
   try {
     const item = await Item.findByPk(req.params.id, {
-      include: [{ model: Category }, { model: Date }],
+      include: [{ model: Category }],
     });
     res.status(200).json({ status: "success", data: item });
   } catch (error) {
@@ -36,6 +40,7 @@ exports.updateItem = async (req, res, next) => {
   try {
     const item = await Item.findByPk(req.params.id);
     await item.update(req.body);
+
     res.status(200).json({ status: "success", data: item });
   } catch (error) {
     next(new AppError(error.message, 400));
@@ -46,6 +51,7 @@ exports.deleteItem = async (req, res, next) => {
   try {
     const item = await Item.findByPk(req.params.id);
     await item.destroy();
+
     res.status(200).json({ status: "success", data: item });
   } catch (error) {
     next(new AppError(error.message, 400));
--- backEnd/controllers/orderController.js
+++ backEnd/controllers/orderController.js
@@ -31,3 +31,25 @@ exports.createOrder = async (req, res, next) => {
     next(new AppError(error.message, 400));
   }
 };
+
+exports.updateOrder = async (req, res, next) => {
+  try {
+    const order = await Order.findByPk(req.params.id);
+    await order.update(req.body);
+
+    res.status(200).json({ status: "success", data: order });
+  } catch (error) {
+    next(new AppError(error.message, 400));
+  }
+};
+
+exports.deleteOrder = async (req, res, next) => {
+  try {
+    const order = await Order.findByPk(req.params.id);
+    await order.destroy();
+
+    res.status(200).json({ status: "success", data: order });
+  } catch (error) {
+    next(new AppError(error.message, 400));
+  }
+};
