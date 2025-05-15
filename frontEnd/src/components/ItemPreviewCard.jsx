a080210980592faea462e13928690e4d5cfb36b0
13urn0ut <nitrojunky@gmail.com>
2025-05-15 18:51:17 +0300

    Add basic ItemPreviewCard style

--- frontEnd/src/components/ItemPreviewCard.jsx
+++ frontEnd/src/components/ItemPreviewCard.jsx
@@ -7,11 +7,13 @@ const ItemPreviewCard = ({ item }) => {
         src={`${VITE_PUBLIC_URL}/imgs/${item.image || "default.jpg"}`}
         alt={item.name}
       />
-      <h3>{item.name}</h3>
-      <p>Price: {item.price}</p>
-      <p>Duration: {item.duration}</p>
-      <p>Rating: {item.rating}</p>
-      <p>Category: {item.category.name}</p>
+      <div>
+        <h3>{item.name}</h3>
+        <p>Price: {item.price}</p>
+        <p>Duration: {item.duration}</p>
+        <p>Rating: {item.rating}</p>
+        <p>Category: {item.category.name}</p>
+      </div>
     </article>
   );
 };
